89ae3df94b4d4febe2ef2af9f375faa9766c1b38
Aniket Vakil <aniketvakil3@gmail.com>
2020-08-22 11:22:11 +0530

    SM links and email

--- src/config.js
+++ src/config.js
@@ -1,38 +1,34 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Aniket Vakil',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Aniket Vakil is a Computer Science student, with a specialization and keen interest in the field of Data Analytics.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
+    'Aniket Vakil, Aniket, Vakil, software engineer, machine learning, data analytics, nirma university, deep learing, neural networks',
   siteUrl: 'https://brittanychiang.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Aniket Vakil',
+  location: 'Vadodara, Gujarat, India',
+  email: 'aniketvakil3@gmail.com',
+  github: 'https://github.com/aniketvakil',
+  twitterHandle: '@AniketVakil',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/aniketvakil',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://www.linkedin.com/in/aniket-vakil/',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/aniketvakil/',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/AniketVakil',
     },
   ],
 
